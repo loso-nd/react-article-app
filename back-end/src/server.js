f52b4b7ef10d09bbd29ed6875bbfd59096a5cd3c
loso-nd <losond8@gmail.com>
2025-10-24 22:39:38 -0500

    Adding comments

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
 // In memory database server to track comments, upvotes
+
 const articleInfo = [
-    { name: 'learn-node', upvotes: 0},
-    { name: 'learn-react', upvotes: 0},
-    { name: 'mongo-db', upvotes: 0}
+  { name: 'learn-node', upvotes: 0, comments: [] },
+  { name: 'learn-react', upvotes: 0, comments: [] },
+  { name: 'mongo-db', upvotes: 0, comments: [] },
 ];
 
 const app = express();
@@ -13,25 +14,34 @@ const port = 8000;
 // Tells our request app if it sees a request body, it should add it to this request.body object.
 app.use(express.json());
 
-// creating a new upvote
-//GET the value of this route param ( name) to find the corresponding article and increment the number of up votes
-app.get('/api/articles/:name/upvote', (req, res) =>  {
-    const article = articleInfo.find(a => a.name === req.params.name);
+
+//GET the value of this route param (name) to find the corresponding article and increment the number of up votes
+app.post('/api/articles/:name/upvote', (req, res) =>  {
+    const { name } = req.params;
+    const article = articleInfo.find(a => a.name === name);
     article.upvotes += 1
 
-    // send back a response
-    res.send('Success! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes!')
+    // send back the udpated article
+    res.json(article);
 });
 
-app.get('/hello/:name', function(req, res) {
-    res.send('Hello World, ' + req.params.name )
+//Endpoint will allow users to add comments to an article.
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articleInfo.find(a => a.name === name);
+
+    article.comments.push({
+        postedBy,
+        text
+    });
+
+    // send back the udpated article
+    res.json(article);
 });
 
-app.post('/hello', function(req, res) {
-    res.send('Hello , ' + req.body.name + ' from a POST endpoint!')
-});
-
-// First arg is the port & Second is a callback function
+// First arg is the port & Second is a callback function or route handler
 app.listen(port, function() {
     console.log('Server is listening on port: 8000')
 })
