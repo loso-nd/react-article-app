2c05080cbe7d7dd452cd09f64b8628f4f37bb64e
loso-nd <losond8@gmail.com>
2025-10-24 22:16:47 -0500

    Add nodemon package

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,15 +1,26 @@
 import express from 'express';
 
+// In memory database server to track comments, upvotes
+const articleInfo = [
+    { name: 'learn-node', upvotes: 0},
+    { name: 'learn-react', upvotes: 0},
+    { name: 'mongo-db', upvotes: 0}
+];
+
 const app = express();
-const port = 8000
+const port = 8000;
 
 // Tells our request app if it sees a request body, it should add it to this request.body object.
 app.use(express.json());
 
-// Define a path to and endpoint to request a return(message or response)
-// 1st arg is the route, 2nd is the callback funciton
-app.get('/hello', function(req, res) {
-    res.send('Hello World, from a GET endpoint!')
+// creating a new upvote
+//GET the value of this route param ( name) to find the corresponding article and increment the number of up votes
+app.get('/api/articles/:name/upvote', (req, res) =>  {
+    const article = articleInfo.find(a => a.name === req.params.name);
+    article.upvotes += 1
+
+    // send back a response
+    res.send('Success! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes!')
 });
 
 app.get('/hello/:name', function(req, res) {
